081562986f75a698517acfa6a28248359f38ca81
Anton <anton9121@gmail.com>
2014-01-16 18:23:22 +0600

    upd

--- twi2rss.js
+++ twi2rss.js
@@ -14,16 +14,22 @@ var read_data = function(data) {
 	var arr = $(data.items_html); //$($.parseHTML(data.items_html));
 	$.each(arr, function(k, item) {
 		var tw_item = {};
-		var tweet = $(item).find('.tweet');
+		var tweet = $(item).find('.tweet').eq(0);
 		if (tweet.length === 0) {
 			return 1;
 		}
 		tw_item.time = $(item).find('._timestamp').data('time');
+		tw_item.is_rp = (tweet.data('is-reply-to')) ? true : false;
+		if (tw_item.is_rp) {
+			return 1;
+		}
 		tw_item.is_rt = (tweet.data('retweeter')) ? true : false;
-		tw_item.text = $(item).find('.tweet-text').html();
+		tw_item.html = $(item).find('.tweet-text').html();
+		tw_item.text = $(item).find('.tweet-text').text();
 		tw_item.user = (tw_item.is_rt)?tweet.data('retweeter'):tweet.data('screen-name');
 		tw_item.author = tweet.data('screen-name');
 		tw_item.id = tweet.data('tweet-id') || tweet.data('item-id');
+		tw_item.fb = tweet.data('feedback-key').replace(/.*_(.*)$/, '$1');
 		tw.push(tw_item);
 	});
 	make_feed(tw);
@@ -37,9 +43,9 @@ var make_feed = function(data) {
 	for (var i = 0, item; item = data[i]; i++) {
 		var date = new Date(item.time * 1000);
 		feed+='<item>';
-		feed+='<title><![CDATA['+((item.is_rt)?'RT by '+item.author:item.author)+', id:'+item.id+']]></title>';
-		feed+='<link><![CDATA[https://twitter.com/bigpodcast/statuses/'+item.id+']]></link>';
-		feed+='<description><![CDATA['+item.text+']]></description>';
+		feed+='<title><![CDATA['+((item.is_rt)?'RT! '+item.author:item.author)+': '+item.text+']]></title>';
+		feed+='<link><![CDATA[https://twitter.com/_/status/'+item.fb+']]></link>';
+		feed+='<description><![CDATA['+item.html+']]></description>';
 		feed+='<pubDate>'+date.toGMTString()+'</pubDate>';
 		feed+='<guid>'+item.id+'</guid>';
 		feed+='<author>'+item.author+'</author>';
