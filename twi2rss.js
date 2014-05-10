679ee6951c43a5ad089499bf74eca91c2c8ed0bc
Anton <anton9121@gmail.com>
2014-05-10 17:14:21 +0600

    fix

--- twi2rss.js
+++ twi2rss.js
@@ -14,16 +14,16 @@ var get_page = function(max_id) {
 };
 get_page();
 var text_parse = function (text, html) {
-    var content = html;
-    var youtube = text.replace(/.*youtu.*v=([^&]*).*/, '$1');
-    if (youtube !== text) {
-        content += '<br/><iframe width="560" height="315" src="//www.youtube.com/embed/'+youtube+'" frameborder="0" allowfullscreen></iframe>';
-    }
-    var instagram = text.replace(/.*instagram\.com\/p\/([^/ ]*).*/,'$1');
-    if (instagram !== text) {
-        content += '<br/><iframe src="//instagram.com/p/'+instagram+'/embed/" width="612" height="710" frameborder="0" scrolling="no" allowtransparency="true"></iframe>'
-    }
-    return content;
+	var content = html;
+	var youtube = text.replace(/.*youtu.*v=([^&]*).*/, '$1');
+	if (youtube !== text) {
+		content += '<br/><iframe width="560" height="315" src="//www.youtube.com/embed/'+youtube+'" frameborder="0" allowfullscreen></iframe>';
+	}
+	var instagram = text.replace(/.*instagram\.com\/p\/([^/ ]*).*/,'$1');
+	if (instagram !== text) {
+		content += '<br/><iframe src="//instagram.com/p/'+instagram+'/embed/" width="612" height="710" frameborder="0" scrolling="no" allowtransparency="true"></iframe>'
+	}
+	return content;
 };
 var read_data = function(data) {
 	var tw = [];
@@ -34,23 +34,45 @@ var read_data = function(data) {
 	var arr = $(data.items_html); //$($.parseHTML(data.items_html));
 	$.each(arr, function(k, item) {
 		var tw_item = {};
-		var tweet = $(item).find('.tweet').eq(0);
+		var $item = $(item);
+		var tweet = $item.find('.tweet').eq(0);
+		var type = 0;
+		if (tweet.length === 0) {
+			tweet = $item.find('.js-tweet').eq(0);
+			type = 1;
+		}
 		if (tweet.length === 0) {
 			return 1;
 		}
-		tw_item.time = $(item).find('._timestamp').data('time');
-		tw_item.is_rp = (tweet.data('is-reply-to')) ? true : false;
-		tw_item.is_rt = (tweet.data('retweeter')) ? true : false;
-		tw_item.html = $(item).find('.tweet-text').html();
-		tw_item.text = $(item).find('.tweet-text').text();
-		tw_item.user = (tw_item.is_rt) ? tweet.data('retweeter') : tweet.data('screen-name');
-		tw_item.author = tweet.data('screen-name');
-		tw_item.id = tweet.data('tweet-id') || tweet.data('item-id');
-		tw_item.fb = tweet.data('feedback-key').replace(/.*_(.*)$/, '$1');
-        var pic = tweet.find('a.is-preview > div.is-preview > div');
-        if (pic.length > 0) {
-            tw_item.html += '<br/><img src="'+pic.data('img-src')+'" width="100%" style="'+pic.attr('style')+'"/>';
-        }
+		if (type === 0) {
+			tw_item.time = $item.find('._timestamp').data('time');
+			tw_item.is_rp = (tweet.data('is-reply-to')) ? true : false;
+			tw_item.is_rt = (tweet.data('retweeter')) ? true : false;
+			tw_item.html = $item.find('.tweet-text').html();
+			tw_item.text = $item.find('.tweet-text').text();
+			tw_item.user = (tw_item.is_rt) ? tweet.data('retweeter') : tweet.data('screen-name');
+			tw_item.author = tweet.data('screen-name');
+			tw_item.id = tweet.data('tweet-id') || tweet.data('item-id');
+			tw_item.fb = tweet.data('feedback-key').replace(/.*_(.*)$/, '$1');
+			var pic = tweet.find('a.is-preview > div.is-preview > div');
+			if (pic.length > 0) {
+				tw_item.html += '<br/><img src="'+pic.data('img-src')+'" width="100%" style="'+pic.attr('style')+'"/>';
+			}
+		} else {
+			tw_item.time = $item.find('.js-short-timestamp').data('time');
+			tw_item.is_rp = (tweet.data('is-reply-to')) ? true : false;
+			tw_item.is_rt = (tweet.data('retweeter')) ? true : false;
+			tw_item.html = $item.find('.js-tweet-text').html();
+			tw_item.text = $item.find('.js-tweet-text').text();
+			tw_item.user = (tw_item.is_rt) ? tweet.data('retweeter') : tweet.data('screen-name');
+			tw_item.author = tweet.data('screen-name');
+			tw_item.id = tweet.data('tweet-id') || tweet.data('item-id');
+			tw_item.fb = tweet.data('feedback-key').replace(/.*_(.*)$/, '$1');
+			var pic = tweet.find('a.TwitterPhoto-link > img');
+			if (pic.length > 0) {
+				tw_item.html += '<br/><img src="'+pic.attr('src')+'" width="100%" style="'+pic.attr('style')+'"/>';
+			}
+		}
 		tw_item.html = text_parse(tw_item.text, tw_item.html);
 		tw.push(tw_item);
 	});
